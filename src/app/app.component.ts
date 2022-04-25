c1c733003fe0177c4eaa4888bfe4656dcfc9119a
Kimberly <kimolanga123@gmail.com>
2022-04-25 13:29:04 +0300

    add nav-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-// import { FormControl, ReactiveFormsModule } from '@angular/forms';
-// import { Subscription } from 'rxjs';
-// import { APIService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -10,38 +7,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'API-P';
-  // mySubscription: Subscription = new Subscription;
-
-  // constructor(private apiService: APIService){
-
-  // }
-
-  // ngOnDestroy(): void {
-  //   this.mySubscription.unsubscribe();
-  // }
-
-  // repos = []
-  // ngOnInit(): void {
-  //   this.getPublicReposWithSubscription()
-  // }
-
-  // getPublicReposWithSubscription(){
-  //   this.mySubscription.add(
-  //     this.apiService.getRepos('Rache1nk').subscribe((user:any)=> console.log(user))
-  //   )
-  // }
-
-  // name = new FormControl()
+  
 }
-
-// async getPublicReposWithPromise(username:string ){
-//   const repos = await this.APIService.getRepo(username)
-//   this.repos = repos
-// }
-
-// searchRepos(){
-//   this.getPublicReposWithPromise(this.name.value)
-//   return false;
-// }
 
 
